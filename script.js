48e65142afe908cde001a1eef87a9f28ccae92b2
anurag-pandey-748805 <anurag.p@ariantechsolutions.com>
2026-09-02 17:04:09 +0530

    Updated portfolio content and styling

--- script.js
+++ script.js
@@ -4,8 +4,17 @@ const closePopUp = document.querySelector('#close-icon')
 
 openPopUp.addEventListener('click',() => {
     aboutMe.classList.remove('hidden')
+    document.body.classList.add('overflow-hidden')
 })
 
 closePopUp.addEventListener('click', () => {
     aboutMe.classList.add('hidden')
+    document.body.classList.remove('overflow-hidden')
+})
+
+aboutMe.addEventListener('click', (event) => {
+    if (event.target === aboutMe) {
+        aboutMe.classList.add('hidden')
+        document.body.classList.remove('overflow-hidden')
+    }
 })
